8a022385247e7d90d8722870f4f4d771e603ba99
Zhai Zhaoxuan <zhaizhaoxuan@xiaomi.com>
2018-06-11 16:44:21 +0800

    remove unused permission

--- background.js
+++ background.js
@@ -1,18 +1,3 @@
-const ignore = {
-    cancel: false
-};
-
-function callback(details) {
-    let url = new URL(details.url);
-    let req_url = url.searchParams.get('requrl');
-    if (!req_url) {
-        return ignore;
-    }
-    return {
-        redirectUrl: req_url,
-    }
-}
-
 function onMessage(msg) {
     switch (msg.cmd) {
         case 'openOptionPage':
@@ -22,10 +7,3 @@ function onMessage(msg) {
 }
 
 chrome.runtime.onMessage.addListener(onMessage);
-chrome.webRequest.onBeforeRequest.addListener(callback,
-    {
-        urls: [
-            "https://cas.mioffice.cn/*"
-        ]
-    },
-    ["blocking"]);
